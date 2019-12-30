8c71e3f3ecaa6c2fd30a1550f1ad26e9904833f2
dengqi1 <dengqi1@lvshou.com>
2019-12-30 10:47:17 +0800

    dengqi

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page 邓琪dengqiabc'),
+      home: MyHomePage(title: 'Flutter Demo Home Page dengqi123'),
     );
   }
 }
